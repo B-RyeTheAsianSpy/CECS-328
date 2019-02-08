8043fbac862a32bfa64be27e4dd4f09dbba9f106
B-RyeTheAsianSpy <33035218+B-RyeTheAsianSpy@users.noreply.github.com>
2019-02-07 17:25:13 -0800

    Update Question2.java

--- Programming Assignments/Lab 2/src/Question2.java
+++ Programming Assignments/Lab 2/src/Question2.java
@@ -3,22 +3,6 @@ import java.util.Scanner;
 public class Question2 {
 
     /**
-     * Given an array of sorted 0's and 1's, we are finding the index of where the split
-     * between the two numbers occur
-     *
-     * @param a - the array that we are searching for the split
-     * @return the index where the split occurs
-     */
-    public static int binarySearch(int a[]){
-        // p is the first element of the array, r is the last element
-        int p = 0, r = a.length - 1;
-        while(p <= r){
-           
-        }
-        return -1;
-    }
-
-    /**
      * @author Brian Nguyen
      * @since 10/11/18
      */
@@ -37,6 +21,19 @@ public class Question2 {
             k = in.nextInt();
         }
 
+        populateArray(a, n, k);
+        printArray(a, n);
+
+        System.out.println("\nThe zeroes and ones split at k = " + binarySearch(a));
+
+    }
+    /**
+     * populates an array with 0's and 1's
+     * @param a - array
+     * @param n - number of 0's
+     * @param k - number of 1's
+     */
+    public static void populateArray(int [] a, int n, int k){
         // populate the array with leading 0's
         for (int i = 0; i <= k; i++) {
             a[i] = 0;
@@ -45,7 +42,14 @@ public class Question2 {
         for (int i = k; i < n; i++) {
             a[i] = 1;
         }
+    }
 
+    /**
+     * prints an array
+     * @param a - the array to be printed
+     * @param n - the size of the array
+     */
+    public static void printArray(int a[], int n){
         // printing the array
         System.out.println("Your array:");
         System.out.print("[ ");
@@ -53,6 +57,32 @@ public class Question2 {
             System.out.print(a[i] + " ");
         }
         System.out.print("]");
-        System.out.println("\nThe zeroes and ones split at k = " + binarySearch(a));
+    }
+    /**
+     * Given an array of sorted 0's and 1's, we are finding the index of where the split
+     * between the two numbers occur
+     *
+     * @param a - the array that we are searching for the split
+     * @return the index where the split occurs
+     */
+    public static int binarySearch(int a[]){
+        // p is the first element of the array, r is the last element
+        int p = 0, r = a.length - 1;
+        while(p <= r){
+            // q is the mid point
+            int q = (p + r) / 2;
+
+            // if the middle element is zero and the element after that is 1, we have found our split
+            if (a[q] == 0 && a[q + 1] == 1) return q + 1;
+
+            // if the middle element is zero and the element after that is still zero, then we remove the first
+            // half of the array
+            if (a[q] == 0 && (a[q] == a[q+1] )) p = q + 1;
+
+            // if the middle element is 1 and the element after that is still 1, then we remove the second
+            // half of the array
+            if (a[q] == 1 && (a[q] == a[q+1])) r = q - 1;
+        }
+        return -1;
     }
 }
