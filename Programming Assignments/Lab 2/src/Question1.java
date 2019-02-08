3ae9a7d4522e4ad62dd2c3bed8fa9863c4616e7a
B-RyeTheAsianSpy <33035218+B-RyeTheAsianSpy@users.noreply.github.com>
2019-02-07 17:24:29 -0800

    Update Question1.java

--- Programming Assignments/Lab 2/src/Question1.java
+++ Programming Assignments/Lab 2/src/Question1.java
@@ -2,6 +2,8 @@ import java.util.Scanner;
 
 public class Question1 {
     /**
+     * Lab 2, Part 1 - Using the idea of the binary search algorithm and finding the square root of an integer
+     *
      * @author Brian Nguyen
      * @since 10/11/18
      *
@@ -27,7 +29,16 @@ public class Question1 {
         if (n < 1) return 0;
 
         // p is the first index of the "array", q is the midpoint, r is the last
-        
+        int p = 0, r = n, q = (p + r) / 2;;
+
+        while (p <= r){
+            q = (p + r) / 2;
+            // COMMENT / UNCOMMENT STATEMENT TO SEE HOW ALGORITHM DIVIDES AND CONQUERS
+            System.out.println(q);
+            // if the midpoint of the "array" is the answer, return the midpoint
+            if ((q*q) == n) return q;
+            if ((q*q) > n) r = q - 1;
+            else p = q + 1;
         }
         // returns the ceiling value of our final element q if q*q != n
         return q+1;
